6a06bce9462dfbe1baef3a56c2261278c40bae30
Caro-26S <sara_sanchez82221@elpoli.edu.co>
2023-10-26 17:06:34 -0500

    clase

--- src/practica/Hotel.java
+++ src/practica/Hotel.java
@@ -1,13 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package practica;
 
-/**
- *
- * @author 57301
- */
 public class Hotel {
+    int IDHotel;
+    String Nombre;
+    String Zona;
+    int Precio;
+
+    public Hotel(int IDHotel, String Nombre, String Zona, int Precio) {
+        this.IDHotel = IDHotel;
+        this.Nombre = Nombre;
+        this.Zona = Zona;
+        this.Precio = Precio;
+    }
+    
+    public Hotel() {
+        this.IDHotel = 0;
+        this.Nombre = "";
+        this.Zona = "";
+        this.Precio = 0;
+    }
+
+    public int getIDHotel() {
+        return IDHotel;
+    }
+
+    public void setIDHotel(int IDHotel) {
+        this.IDHotel = IDHotel;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getZona() {
+        return Zona;
+    }
+
+    public void setZona(String Zona) {
+        this.Zona = Zona;
+    }
+
+    public int getPrecio() {
+        return Precio;
+    }
+
+    public void setPrecio(int Precio) {
+        this.Precio = Precio;
+    }
     
 }
