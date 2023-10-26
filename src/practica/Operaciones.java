6dafea2ef367e43a02d31cd65d15bd15d6c74e8b
11JuanK11 <juan_alzate82221@elpoli.edu.co>
2023-10-26 18:16:15 -0500

    lectura archivo

--- src/practica/Operaciones.java
+++ src/practica/Operaciones.java
@@ -4,10 +4,34 @@
  */
 package practica;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.*;
+
 /**
  *
  * @author Usuario
  */
 public class Operaciones {
+    private static File F = new File("./src/practica/Information/Information.txt");
     
+    public static Set Recuperar(){
+        Set<Hotel> conj = new HashSet();
+        try {
+            FileReader reader = new FileReader(F);
+            BufferedReader buffer = new BufferedReader(reader);
+            String S = "", vect[];
+            while (S != null) {     
+                S = buffer.readLine();
+                vect = S.split(";");
+                if (S != null) {
+                    Hotel obj = new Hotel(Integer.parseInt(vect[0]), vect[1], vect[2],Integer.parseInt(vect[3]) );
+                    conj.add(obj);
+                }
+            }
+            buffer.close();
+        } catch (Exception e) {}
+        return conj;
+    }
 }
